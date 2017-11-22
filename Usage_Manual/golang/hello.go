11cc56fd0364460e1b42fff7cb49af00816acd17
7900ms <7900ms@users.noreply.github.com>
2017-11-22 07:41:00 +0000

    Create hello.go

---
+++ Usage_Manual/golang/hello.go
@@ -0,0 +1,17 @@
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+
+// 1
+// go build hello-world.go (cpp式)
+// ./hello-world
+// 2
+// go run hello-world.go  (脚本式)
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+    fmt.Println("gday")
+}
